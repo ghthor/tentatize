783cb0483bc9a050df1251749cdd0eb50e00b174
Wilhelmina Drengwitz <ghthor@gmail.com>
2015-04-13 14:10:57 -0400

    [cmd] add cli and daemon commands

---
+++ cmd/tentatize/main.go
@@ -0,0 +1,12 @@
+// tentatize is an cli application to manage
+// a tentative schedule/planner.
+// The storage format will be human readable
+// and writable and should be convient to edit
+// directly using their normal text editor(ex: vim).
+// The data is stored as text files and the
+// changes over time will be monitored and
+// recorded in a git repository.
+package main
+
+func main() {
+}
---
+++ cmd/tentatized/main.go
@@ -0,0 +1,9 @@
+// tentatized is a daemon that will monitor a
+// tentatize schedule directory. It will send notifications,
+// transistion the schedule through time. tentatized will also
+// provide a web interface and an api that can be used by
+// web/mobile applications to interact with the schedule.
+package main
+
+func main() {
+}
